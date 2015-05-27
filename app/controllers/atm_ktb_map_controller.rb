597fc5fce1df3e22280e9fa6f3d584494f5c537d
sarawoot manmor <kun_a44@hotmail.com>
2015-05-27 12:48:36 +0700

    update atm layer

--- app/controllers/atm_ktb_map_controller.rb
+++ app/controllers/atm_ktb_map_controller.rb
@@ -5,9 +5,19 @@ class AtmKtbMapController < ApplicationController
   def index
     @atm_ktb = [];
     if params[:code].present?
-      @atm_ktb = AtmKtb.select("location, longitude, latitude")
-                       .where(machinecod: params[:code])
-                       .first
+      res = RestClient.post 'http://172.16.7.15/dsi-i2-ws/map.asmx/GetBankByMachineCode', 
+                            {machine_code: params[:code]}, 
+                            :content_type => :json, 
+                            :accept => :json
+      @atm_ktb = JSON.parse(res.body)[0]
+      if @atm_ktb["bank_code"] == '004'
+        coordinate = Coordinates.utm_to_lat_long("WGS-84", 
+                                  @atm_ktb["longitude"], 
+                                  @atm_ktb["latitude"], 
+                                  "47N")
+        @atm_ktb["longitude"] = coordinate[:long]
+        @atm_ktb["latitude"] = coordinate[:lat]
+      end
     end
     if @atm_ktb.blank?
       render text: "กรุณาระบุรหัสเครื่อง"
