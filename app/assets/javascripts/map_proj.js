14a2d57bc2d3f8da2ec33abddd6591f66c79b2b3
root <root@map.dsi.go.th>
2017-01-31 06:48:25 +0700

    update

--- app/assets/javascripts/map_proj.js
+++ app/assets/javascripts/map_proj.js
@@ -1,6 +1,11 @@
-Proj4js.defs["EPSG:32647"] = "+proj=utm +zone=47 +ellps=WGS84 +datum=WGS84 +units=m +no_defs";
-Proj4js.defs["EPSG:32648"] = "+proj=utm +zone=48 +ellps=WGS84 +datum=WGS84 +units=m +no_defs";
-Proj4js.defs["EPSG:24047"] = "+proj=utm +zone=47 +a=6377276.345 +b=6356075.41314024 +units=m +no_defs";
-Proj4js.defs["EPSG:24048"] = "+proj=utm +zone=48 +a=6377276.345 +b=6356075.41314024 +units=m +no_defs";
-Proj4js.defs["EPSG:4326"] = "+proj=longlat +datum=WGS84 +no_defs "
-Proj4js.defs["EPSG:900913"] = "+proj=merc +a=6378137 +b=6378137 +lat_ts=0.0 +lon_0=0.0 +x_0=0.0 +y_0=0 +k=1.0 +units=m +nadgrids=@null +no_defs"
+Proj4js.defs["EPSG:4326"] = "+proj=longlat +datum=WGS84 +no_defs";
+Proj4js.defs["EPSG:32647"] = "+proj=utm +zone=47 +datum=WGS84 +units=m +no_defs";
+Proj4js.defs["EPSG:32648"] = "+proj=utm +zone=48 +datum=WGS84 +units=m +no_defs";
+Proj4js.defs["EPSG:24047"] = "+proj=utm +zone=47 +a=6377276.345 +b=6356075.41314024 +towgs84=210,814,289,0,0,0,0 +units=m +no_defs";
+Proj4js.defs["EPSG:24048"] = "+proj=utm +zone=48 +a=6377276.345 +b=6356075.41314024 +towgs84=210,814,289,0,0,0,0 +units=m +no_defs";
+Proj4js.defs["EPSG:3857"] = "+proj=merc +a=6378137 +b=6378137 +lat_ts=0.0 +lon_0=0.0 +x_0=0.0 +y_0=0 +k=1.0 +units=m +nadgrids=@null +wktext  +no_defs";
+Proj4js.defs["EPSG:900913"] = "+proj=merc +a=6378137 +b=6378137 +lat_ts=0.0 +lon_0=0.0 +x_0=0.0 +y_0=0 +k=1.0 +units=m +nadgrids=@null +wktext  +no_defs";
+
+
+
+
