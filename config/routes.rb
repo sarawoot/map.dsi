eb8a6b0c138a7f91a87d533fc5699236b6970382
root <root@map.dsi.go.th>
2017-10-30 09:42:37 +0700

    update

--- config/routes.rb
+++ config/routes.rb
@@ -5,9 +5,9 @@ Rails.application.routes.draw do
 
   devise_for :users
 
-  #devise_scope :user do
-  #  get "/login" => "devise/sessions#new"
-  #end
+  devise_scope :user do
+    get "/login" => "devise/sessions#new"
+  end
 
   # as :user do
   #   get "/login" => "devise/sessions#new"
